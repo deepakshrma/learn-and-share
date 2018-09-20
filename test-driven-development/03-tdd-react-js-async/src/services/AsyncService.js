913268fb1bd162d019b9a2e14177034965e9ce63
xdeepakv <deepak.m.shrma@gmail.com>
2018-09-21 01:42:51 +0800

    added axios test cases

--- test-driven-development/03-tdd-react-js-async/src/services/AsyncService.js
+++ test-driven-development/03-tdd-react-js-async/src/services/AsyncService.js
@@ -1,6 +1,17 @@
+import axios from 'axios';
+async function asyncAxiosData() {
+    console.log('get data');
+    const result = await axios.get(
+        "https://api.coindesk.com/v1/bpi/currentprice.json"
+    );
+    return result;
+}
+
 const asyncData = () => {
     return Promise.resolve("async data")
 }
+
 export {
-    asyncData
+    asyncData,
+    asyncAxiosData
 }
